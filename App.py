3243eed054e0bfb264f43eec5471e3ba9a9ed47e
Bushra Akram <141483100+BushraAkram111@users.noreply.github.com>
2024-08-28 07:50:25 +0500

    Update App.py

--- App.py
+++ App.py
@@ -3,10 +3,10 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
-import streamlit as st
+import matplotlib
 import pandas as pd
+import numpy as np
 import seaborn as sns
-
 # App title
 st.title("Enhanced Data Visualization and Analysis App")
 
